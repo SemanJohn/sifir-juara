c27783695d7e65af75f9ced02f0ca0fdb0a82cca
SemanJohn <barukulamamu@gmail.com>
2026-07-21 20:04:24 +0800

    Papan markah papar Memuatkan semasa data belum siap (v1.6)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker Sifir Juara — cache untuk main offline
-var CACHE = "sifir-juara-v24";
+var CACHE = "sifir-juara-v25";
 var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
