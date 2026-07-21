edf647f7c3cd57422a28c5ca966e7c16db9b8caa
SemanJohn <barukulamamu@gmail.com>
2026-07-21 23:08:59 +0800

    Betulkan papan markah VS ditimpa poll biasa (v2.8)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker Sifir Juara — cache untuk main offline
-var CACHE = "sifir-juara-v36";
+var CACHE = "sifir-juara-v37";
 var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
